b098f57f3ba08c290fb2b9ce6a507d9c26be9b86
Sanjiva Weerawarana <sanjiva@weerawarana.org>
2020-08-05 17:11:13 +0530

    Last fix done to deal with backwards logic for app - need to undo later

--- datagen/afterdataload.sql
+++ datagen/afterdataload.sql
@@ -1,4 +1,4 @@
 USE epollbook;
 
 INSERT INTO VoteRecords (Election, DistrictID, PollingDivisionID, PollingStationID, ID)
-SELECT "PE2020", DistrictID, PollingDivisionID, PollingStationID, ID FROM ElectorRegistry;
+SELECT "PE2020", DistrictID, PollingDivisionID, PollingStationID, ElectorID FROM ElectorRegistry;
